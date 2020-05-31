1e5d4806c27f35cab9038d143b17db177cc31eec
anton_tesliuk <anton.tesliuk@gmail.com>
2020-05-31 10:38:20 +0300

    Initial empty project

---
+++ src/main/java/com/atesliuk/investing_simulator/InvestingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.atesliuk.investing_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestingSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atesliuk/investing_simulator/InvestingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atesliuk.investing_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
